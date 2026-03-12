8b62b53332c5d01c2a794311795ee224181f2335
KelvinTayag <krbtayag.student@ua.edu.ph>
2026-03-12 11:53:27 +0800

    session14

---
+++ session14/index.js
@@ -0,0 +1,90 @@
+// This is how we print output using JS
+// console.log(stringConten|Variable)
+console.log("Hello World!");
+
+// To use comment we need to use -> //
+//  Single line comment
+
+/*
+Multi lines comments
+*/
+
+// [SECTION] VARIABLES  to store data
+
+// Acts as a storage for data which can be modified afterwards except constant variables
+
+// [SECTION] DATA TYPES
+
+// Numeric Data Types
+
+/*
+
+let variable name; -> no initialized value
+let variable name = value; -> with initialized value
+
+
+*/
+
+let age;
+let height = 160;
+
+console.log(height);
+
+// JS is a loose-type PL
+console.log(height)
+
+
+console.    log (height )
+
+console
+
+
+.
+
+log
+
+(height)
+// Synchronous PL -> JS
+
+let gwa = 99.75;
+
+console.log(gwa);
+
+// Character datatype
+
+let firstName = "John"
+
+console.log(firstName);
+
+// typef=of -> to check a variable's type of data
+
+console.log(typeof firstName);
+console.log(typeof gwa);
+console.log(typeof height);
+
+// text casing in JS
+//  Camel casing -> thisIsCamelCasing
+// Snake Casing -> this_is_snake_case
+// Pascal Case -> ThisIsPascalCase
+// Kebab Case -> this-is-kebab-case
+// Pasacal Kebab Case -> This-Is-kebab-case
+
+// Boolean Data types
+
+let isMarried = false;
+let isRegistered = true;
+let hasExpired = true;
+
+console.log(isMarried);
+console.log(isMarried);
+
+console.log(typeof isRegistered)
+
+// Concatenation 
+let province = "Pampanga";
+console.log("I live in " + province)
+
+
+//Type-Coercion
+//Concatenation of string and number or other data types will be all converted to string.
+console.log("My overall grade is: " + gwa)
