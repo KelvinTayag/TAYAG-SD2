ad036573f1eef012566c5272224b0747e608c8f2
KelvinTayag <krbtayag.student@ua.edu.ph>
2026-05-12 19:53:29 +0800

    Added session23

--- session23/index.js
+++ session23/index.js
@@ -1,4 +1,4 @@
-//import the installed module of express 
+//import the installed module of express
 const express = require("express");
 // import mongoose module
 const mongoose = require("mongoose");
@@ -8,8 +8,10 @@ const server = express();
 const port = 5000;
 
 // Trigger connection to mongoDB thru mongoose
-mongoose.connect("mongodb://localhost:27017/");
-// mongoose.connect("mongodb+srv://admin:admin123@isad-db.re65v.mongodb.net/");
+// mongoose.connect("mongodb://localhost:27017/");
+
+mongoose.connect(
+    "mongodb+srv://admin:admin123@sd2-tayag.9jyx2hx.mongodb.net/task-management?appName=SD2-TAYAG");
 
 let db = mongoose.connection;
 
@@ -23,55 +25,155 @@ db.once("open", () => console.log("MongoDB Atlas Connection Succcess!"));
 
 const taskSchema = new mongoose.Schema({
     name: String,
+    description: String,
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+    dateAdded: {
+        type: Date,
+        default: Date.now,
+    },
+    dateCompleted: Date,
     status: {
         type: String,
-        default: "pending"
-    }
-})
+        default: "pending",
+    },
+});
 
 //Model
 const Task = mongoose.model("Task", taskSchema);
 
 // Middewares
-server.use(express.json());
-server.use(express.urlencoded({extended: true}));
+server.use(express.json()); // only accepting json format
+server.use(express.urlencoded({ extended: true }));
 
-
+// req -> user, res -> server gives respond so the user knows that it already added.
 server.get("/home", (req, res) => {
     res.send("Hello from the home endpoint!");
 });
 
 server.get("/error", (req, res) => {
     res.status(404).send({
-        code: 404,
-        message: "Sorry the page cannot be found."
-    })
-})
+    code: 404,
+    message: "Sorry the page cannot be found.",
+    });
+});
 
 server.post("/tasks/add", (req, res) => {
     Task.findOne({ name: req.body.name }).then((result, err) => {
-        if (result.name != null && result.name == req.body.name) {
+        if (result != null && result.name == req.body.name) {
         res.send("Duplicate found. This task cannot be added!");
         } else {
-            let newTask = new Task({
-                name: req.body.name,
+        let newTask = new Task({
+            name: req.body.name,
+            description: req.body.description
+        });
+
+        newTask.save().then((savedTask, saveErr) => {
+            if (saveErr) {
+            res.send("There is an error saving the task.");
+            } else {
+            res.status(201).send({
+                code: 201,
+                message: "Task is now added!",
+                data: savedTask
             });
-            newTask.save().then((savedTask, saveErr) => {
-                if (saveErr) {
-                res.send("There is an error saving the task.");
-                } else {
-                    res.status(201).send({
-                        code: 201,
-                        message: "Task is now added!",
-                        data: savedTask,
-                    });
-                }
-            });
+            }
+        });
         }
     });
 });
 
+// server.put("/tasks/:id", (req, res) => {
+//     Task.findById(req.params.id).then((task, err) => {
+//         if (err || !task) {
+//             res.send("Task not found.");
+//         } else {
+//             task.name = req.body.name;
+//             task.description = req.body.description;
+//             task.save().then((updatedTask, saveErr) => {
+//                 if (saveErr) {
+//                     res.send("Error updating task.");
+//                 } else {
+//                     res.status(200).send({
+//                         code: 200,
+//                         message: "Task updated!",
+//                         data: updatedTask
+//                     });
+//                 }
+//             });
+//         }
+//     });
+// });
 
+server.post("/tasks/edit/:taskId", (req, res) => {
+    Task.findOne({_id: req.params.taskId}).then((result, err) => {
+        if(result == null){
+            res.send("Task not found. Cannot edit!");
+        }else{
+            result.name = req.body.name;
+            result.description = req.body.description;
 
+            result.save().then((updatedTask, updateErr) => {
+                if(updateErr){
+                    res.send("There is an error updating the task.");
+                }else{
+                    res.status(200).send({
+                        code: 200,
+                        message: "Task is now updated!",
+                        data: updatedTask
+                    });
+                }
+            })
+        }
+    })
+})
 
-server.listen(port, () => console.log(`Server is now running at port ${port}.`))
+server.post("/tasks/:taskId/mark-complete", (req, res) => {
+    Task.findOne({_id: req.params.taskId}).then((result, err) => {
+        if(result == null){
+            res.send("Task not found. Cannot mark as complete!");
+        }else{
+            result.status = "complete";
+            result.dateCompleted = Date.now();
+
+            result.save().then((completedTask, completeErr) => {
+                if(completeErr){
+                    res.send("There is an error completing the task.");
+                }else{
+                    res.status(200).send({
+                        code: 200,
+                        message: "Task is now marked complete!",
+                        data: completedTask
+                    });
+                }
+            })
+        }
+    })
+})
+
+// server.post("/tasks/:taskId/mark-complete", (req, res) => {
+//     Task.findOne({_id: req.params.taskId}).then((result, err) => {
+//         if(result == null){
+//             res.send("Task not found. Cannot mark as complete!");
+//         }else{
+//             result.status = "complete";
+//             result.dateCompleted = new Date();
+
+//             result.save().then((updatedTask, updateErr) => {
+//                 if(updateErr){
+//                     res.send("There is an error completing the task.");
+//                 }else{
+//                     res.status(200).send({
+//                         code: 200,
+//                         message: "Task is now marked as complete!",
+//                         data: updatedTask
+//                     });
+//                 }
+//             })
+//         }
+//     })
+// })
+
+server.listen(port, () =>console.log(`Server is now running at port ${port}.`));
